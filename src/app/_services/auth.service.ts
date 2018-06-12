f907c42e21989382ccbc3e6fe21b5cfebea16193
Igor Bondarenko <npro27@yandex.ru>
2018-06-12 22:20:00 +1000

    added client login and register

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,34 @@
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { map, delay, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  baseUrl = 'http://localhost:5000/api/auth/';
+  userToken: any;
+  constructor(private http: Http) {}
+
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'login', model, this.requestOptions()).pipe(map((response: Response) => {
+      const user = response.json();
+      if (user) {
+        localStorage.setItem('token', user.tokenString);
+        this.userToken = user.tokenString;
+      }
+    }));
+  }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model,  this.requestOptions());
+  }
+
+  private requestOptions(){
+    const headers = new Headers({ 'Content-type': 'application/json'});
+    return new RequestOptions({ headers: headers});
+  }
+
+}
+
+
